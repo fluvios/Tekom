a2f1c53ea56461a50b28748c2dfc95f9b26f499d
Fachriannoor <noorfachrian@gmail.com>
2015-11-01 15:44:54 +0700

    commit 8
    masih ada beberapa bug, jadi harap diperbaiki lagi

--- src/tekom/Lexic.java
+++ src/tekom/Lexic.java
@@ -20,11 +20,20 @@ public class Lexic {
     private int i, countG, countEG;
     private String token = "";
 
+    /**
+     * Method untuk membaca inputan dari GUI I : String dari GUI text O :
+     * Variabel expresi yang berisi string yang diakhirnya ditambahkan spasi
+     */
     public void Read(String words) {
 //        System.out.println("Masukan ekspresi: ");
         expression = words + " ";
     }
 
+    /**
+     * Method untuk menganalisa leksik setiap karakter pada indeks string dari
+     * ekspersi I : variabel string O : string yang memenuhi kondisi suatu
+     * leksik,tipe leksiknya dan nilai token leksik
+     */
     public void Analyze(int j) {
         token = "";
 
@@ -92,7 +101,7 @@ public class Lexic {
                     /**
                      * pembacaan real dengan aturan floating point
                      */
-                } else if (expression.charAt(j) == 'e' || expression.charAt(j) == 'E') {
+                } else if ((expression.charAt(j) == 'e' || expression.charAt(j) == 'E') && (expression.charAt(j + 1) != ' ')) {
                     token += expression.charAt(j);
                     j++;
                     while (isInteger(expression.charAt(j))) {
@@ -121,6 +130,18 @@ public class Lexic {
                                 j = expression.length() - 1;
                             }
                         }
+                    }
+                } else if ((expression.charAt(j) == '.' || expression.charAt(j) == ',') && (!isInteger(expression.charAt(j + 1)))) {
+                    j++;
+                    if (j != (expression.length() - 1)) {
+                        Analyze(j);
+                        j = expression.length() - 1;
+                    }
+                } else if ((expression.charAt(j) == 'e' || expression.charAt(j) == 'E') && (expression.charAt(j + 1) == ' ')) {
+                    j++;
+                    if (j != (expression.length() - 1)) {
+                        Analyze(j);
+                        j = expression.length() - 1;
                     }
                 } else {
                     lexic.add(new Token(token, "Integer", 3));
@@ -232,12 +253,20 @@ public class Lexic {
         }
     }
 
+    /**
+     * Method untuk menampilkan hasil dari method analyze I : - O : string yang
+     * memenuhi kondisi suatu leksik,tipe leksiknya dan nilai token leksik
+     */
     public void Print() {
         for (Token x : lexic) {
             System.out.println(x.getItem() + ", Lexic Type: " + x.getLexicType() + ", Lexic Value: " + x.getLexicToken());
         }
     }
 
+    /**
+     * Method untuk melakukan pengecekan karakter apakah bernilai integer I :
+     * karakter dari string O : boolean
+     */
     public boolean isInteger(char Token) {
         status = false;
         if (('1' == Token) || ('2' == Token) || ('3' == Token) || ('4' == Token) || ('5' == Token)
@@ -249,10 +278,18 @@ public class Lexic {
         }
     }
 
+    /**
+     * Method untuk melakukan pengecekan karakter apakah bernilai variabel I :
+     * karakter dari string O : boolean
+     */
     public boolean isVariable(char Token) {
         return Character.isAlphabetic(Token);
     }
 
+    /**
+     * Method untuk melakukan pengecekan karakter apakah merupakan operator
+     * tambah I : karakter dari string O : boolean
+     */
     public boolean isPlus(char Token) {
         status = false;
         if (('+' == Token)) {
@@ -263,6 +300,10 @@ public class Lexic {
         }
     }
 
+    /**
+     * Method untuk melakukan pengecekan karakter apakah merupakan operator
+     * kurang I : karakter dari string O : boolean
+     */
     public boolean isMinus(char Token) {
         status = false;
         if (('-' == Token)) {
@@ -273,6 +314,10 @@ public class Lexic {
         }
     }
 
+    /**
+     * Method untuk melakukan pengecekan karakter apakah merupakan operator kali
+     * I : karakter dari string O : boolean
+     */
     public boolean isMultiple(char Token) {
         status = false;
         if (('*' == Token)) {
@@ -283,6 +328,10 @@ public class Lexic {
         }
     }
 
+    /**
+     * Method untuk melakukan pengecekan karakter apakah merupakan operator bagi
+     * I : karakter dari string O : boolean
+     */
     public boolean isDivide(char Token) {
         status = false;
         if (('/' == Token)) {
@@ -293,6 +342,10 @@ public class Lexic {
         }
     }
 
+    /**
+     * Method untuk melakukan pengecekan karakter apakah merupakan kurung buka I
+     * : karakter dari string O : boolean
+     */
     public boolean isGSymbol(char Token) {
         status = false;
         if ('(' == Token) {
@@ -303,6 +356,10 @@ public class Lexic {
         }
     }
 
+    /**
+     * Method untuk melakukan pengecekan karakter apakah merupakan kurung tutup
+     * I : karakter dari string O : boolean
+     */
     public boolean isEGSymbol(char Token) {
         status = false;
         if (')' == Token) {
@@ -313,6 +370,10 @@ public class Lexic {
         }
     }
 
+    /**
+     * Method untuk melakukan pengecekan karakter apakah merupakan eksponen dari
+     * floating point I : karakter dari string O : boolean
+     */
     public boolean isFloat(char Token) {
         status = false;
         if ('+' == Token || '-' == Token) {
